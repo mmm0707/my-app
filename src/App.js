91850d73e1fe171f9a479739e46fcfe311604d5d
mmm0707 <jet4love26love@gmail.com>
2021-12-15 20:05:40 +0900

    add p

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from './Form'
+
+const App = () => {
+    return (
+        <>
+        <h1>Daily App</h1>
+        <Form />
+        </>
+        )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import ReactDOM  from "react-dom";
-import React from 'react';
+import ReactDOM  from "react-dom"
+import React from 'react'
+import App from './App.js'
+import Form from './Form.js'
 
-ReactDOM.render(<h1>アプリ</h1>,
-document.getElementById('root'))
+ReactDOM.render(
+    <App />,
+document.getElementById('root')
+)
